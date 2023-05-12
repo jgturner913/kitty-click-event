58291c2900e04e32ed7e27dde8d10bdde03934e6
J. Turner <113813887+jgturner913@users.noreply.github.com>
2023-05-12 01:01:27 -0500

    Added initial JS script

---
+++ js/script.js
@@ -0,0 +1,8 @@
+var button = document.querySelector(".show-cat");
+var cat = document.querySelector(".cat");
+
+button.addEventListener("click", function () {
+  cat.classList.add("show");
+});
+
+console.log(cat);
